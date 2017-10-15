ee42bcd35eafd5adf6a17dfdb55b7d1537ff5016
UnicodeMan <lveitas956@gmail.com>
2017-10-15 20:05:22 +0300

    Rename Psl. 21/10.cpp to Psl. 21-22/10.cpp

---
+++ Psl. 21-22/10.cpp
@@ -0,0 +1,30 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	double m, s, ats;
+	int lt, ct;
+
+	cout << "Kiek kg saldainiu? ";// << endl;
+	cin >> m;
+
+	cout << "Uz tuos saldainius sumokejai? ";
+	cin >> s;
+
+	ats = s / m;
+
+
+
+
+
+
+	return 0;
+}
