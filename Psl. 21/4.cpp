35537860904afe32a9fe9db9f2b2cea1cd74476f
UnicodeMan <lveitas956@gmail.com>
2017-10-15 19:43:03 +0300

    Add files via upload

---
+++ Psl. 21/4.cpp
@@ -0,0 +1,26 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+    setlocale (LC_CTYPE,"");
+	int a, p, e, sum;
+	cout << "Keik rieðutø atsyneði Juons? ";// << endl;
+	cin >> a;
+
+	p = a + 20;// Petrioks atsineði 20 reiðutø daugiau
+    e = a * 2; // jelena atsyneði dviguba daugiau ne petriuks
+
+    sum = a + p + e; // kadangi uþdavinys praða sumuos visø trijø vakø, tai tep i darau
+
+    cout << sum;
+
+	return 0;
+}
---
+++ Psl. 21/9.cpp
@@ -0,0 +1,22 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	int tc, tf;
+	cout << "tc: ";// << endl;
+	cin >> tc ;
+
+	tf = (9 * tc + 160) / 5;
+
+	cout << tf;
+
+	return 0;
+}
