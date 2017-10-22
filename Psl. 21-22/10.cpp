2b26499ee8a24306cdbc949f0cf88e6b545cdf59
UnicodeMan <lveitas956@gmail.com>
2017-10-22 13:26:42 +0300

    Update 10.cpp

--- Psl. 21-22/10.cpp
+++ Psl. 21-22/10.cpp
@@ -10,21 +10,26 @@
 using namespace std;
 int main ()
 {
-	double m, s, ats;
-	int lt, ct;
+	double m, s, ats; // kiek kainavo saldainiai ir kiek kilogramu pirkai reikes ivesti su tasku, pvz 12.50 lt, todel naudoju double
+	int lt, ct; // paverciant litais ir centais reikes sveiku skaiciu (be tasko)
 
-	cout << "Kiek kg saldainiu? ";// << endl;
-	cin >> m;
+	cout << "Kiek kg saldainiu? ";
+	cin >> m; // kint. "m" yra double
 
-	cout << "Uz tuos saldainius sumokejai? ";
-	cin >> s;
+	cout << "Uz tuos saldainius sumokejai, litais? ";
+	cin >> s; // kint. "s" yra double
+
 
 	ats = s / m;
+	ct = ats * 100; // atsakyma paverciam i centus
+	lt = ct / 100; // surandam, kiek is tu centu iseitu pilnu litu
+	ct = ct - lt * 100; // suskaiciuojam centu likuti is visu centu atemus litus (lt * 100, kad paverst i centus)
 
-
+    cout << "1kg saldainiu kainuos " << lt << " lt ir " << ct << " ct.";
 
 
 
 
 	return 0;
 }
+
