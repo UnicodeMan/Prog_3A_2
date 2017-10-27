d8d3f4e7344378efe65264a04f3f328ae0f02b6c
UnicodeMan <lveitas956@gmail.com>
2017-10-27 11:39:20 +0300

    Add files via upload

---
+++ Psl. 28-30/1.cpp
@@ -0,0 +1,30 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	int uj, up;
+
+	cout << "Jonuko ugis centimetrais: ";// << endl;
+	cin >> uj;
+
+    cout << "Petriuko ugis centimetrais: ";// << endl;
+	cin >> up;
+
+	// Check if Jonukas is taller, then output "taip"
+
+	if(uj > up){
+        cout << "Taip";
+	}
+	else {
+        cout << "Ne";
+	}
+	return 0;
+}
---
+++ Psl. 28-30/2.cpp
@@ -0,0 +1,28 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	int a;
+
+	cout << "Astute, koki skaiciuka gavai? ";// << endl;
+	cin >> a;
+
+	/* Tikrinam */
+
+	if ((a - 25) >= 0) {
+        cout << "Taip";
+	}
+	else {
+        cout << "Ne";
+	}
+
+	return 0;
+}
