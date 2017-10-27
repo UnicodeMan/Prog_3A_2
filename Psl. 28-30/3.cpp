de48ad8d20a18f3337fc9e2a1d1a3ab683d8edb7
UnicodeMan <lveitas956@gmail.com>
2017-10-27 11:42:57 +0300

    Add files via upload

---
+++ Psl. 28-30/3.cpp
@@ -0,0 +1,26 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	int a, b;
+	cout << "Rasyk skaiciu a: ";// << endl;
+	cin >> a;
+
+	if (a >= 0) {
+        b = a + 1;
+	}
+	else {
+        b = a - 3;
+	}
+	cout << b;
+
+	return 0;
+}
