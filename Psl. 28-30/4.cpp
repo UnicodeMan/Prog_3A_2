ceb692cc13758a04c0c13e767b216346ee89e92a
UnicodeMan <lveitas956@gmail.com>
2017-10-27 11:46:55 +0300

    Add files via upload

---
+++ Psl. 28-30/4.cpp
@@ -0,0 +1,28 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	int v1, v2;
+
+	cout << "Pirmojo automobilio greitis, km/h: ";// << endl;
+	cin >> v1;
+
+    cout << "Antrojo automobilio greitis, km/h: ";// << endl;
+	cin >> v2;
+
+	if(v1 > v2) {
+        cout << "Taip";
+	}
+	else {
+        cout << "Ne";
+	}
+	return 0;
+}
