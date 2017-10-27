3843b8464079760c8b14e72cde98305dda7ff2c0
UnicodeMan <lveitas956@gmail.com>
2017-10-27 11:51:14 +0300

    Add files via upload

---
+++ Psl. 28-30/5.cpp
@@ -0,0 +1,29 @@
+// Vieta programos vardui áraðyti
+
+# include <iostream>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <time.h>
+# include <cstdlib>
+
+using namespace std;
+int main ()
+{
+	int a, b;
+
+	cout << "Sk. a: ";// << endl;
+	cin >> a;
+
+	cout << "Sk. b: ";
+	cin >> b;
+
+	if (a <= b) {
+        cout << a << " " << b;
+	}
+	else {
+        cout << b << " " << a;
+	}
+
+	return 0;
+}
